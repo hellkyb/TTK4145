1888c74abd89ecebe56c212e185fcb82a8072b6d
hellkyb <ohelbak@gmail.com>
2017-01-18 13:39:25 +0100

    Update to final ov2 in c , yeeeeeee

--- Exercises/Exercise2/ov2.c
+++ Exercises/Exercise2/ov2.c
@@ -2,12 +2,15 @@
 #include <pthread.h>
 
 int i = 0;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void* someThread()
 {
-	for(int j = 0; j < 10000; j++)
+	for(int j = 0; j < 1000000; j++)
 	{
+		pthread_mutex_lock(&mutex);
 		i++;
+		pthread_mutex_unlock(&mutex);
 	}
 
 	return NULL;
@@ -15,25 +18,29 @@ void* someThread()
 
 void* someThreadTwo()
 {
-	for(int j=0; j < 10000; j++)
+	for(int j=0; j < 1000001; j++)
 	{
+		pthread_mutex_lock(&mutex);
 		i--;
+		pthread_mutex_unlock(&mutex);
 	}
 	return NULL;
 }
 
 int main()
 {
+	
+
 	pthread_t someOtherThread;
-	pthread_create(&someOtherThread, NULL, someThread, NULL);
-	
-	pthread_join(someOtherThread, NULL);
-	
 	pthread_t someOtherThread2;
+	pthread_create(&someOtherThread, NULL, someThread, NULL);
 	pthread_create(&someOtherThread2, NULL, someThreadTwo, NULL);
 	
+	pthread_join(someOtherThread, NULL);
 	pthread_join(someOtherThread2, NULL);
 	printf("%d\n", i);
 
 	return 0;
 }
+
+// Semaphores or Mutex ? - Mutex
